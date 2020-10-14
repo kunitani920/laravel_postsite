a1a3cdb2cf6215d2bdab1485eceaf0ee2be4cf53
tani <kuni.tani.920@gmail.com>
2020-10-14 21:34:54 +0900

    シーダー追加

---
+++ app/my-laravel-app/database/seeds/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      //一般ユーザー（ログイン用)
+      factory(App\User::class)
+      ->create([
+          'name' => 'tani',
+          'email' => 'tani@example.com',
+          'password' => bcrypt('tanikuni')
+      ]);
+      // 一般ユーザー
+      factory(App\User::class, 50)->create();
+    }
+}
