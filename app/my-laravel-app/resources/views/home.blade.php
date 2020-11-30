ce97ff5492c7d667498afa5682989db8e5208bf9
tani <kuni.tani.920@gmail.com>
2020-11-30 21:26:36 +0900

    css変更

--- app/my-laravel-app/resources/views/home.blade.php
+++ app/my-laravel-app/resources/views/home.blade.php
@@ -3,29 +3,13 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-
-                @if(isset($posts))
-                  
-                  @foreach($posts as $post)
-                    <img src="{{ asset($post->images[0] ->icon) }}" alt="..." class="img-thumbnail">
-                  @endforeach
-                  
-                @endif
-            </div>
-        </div>
+      @if(isset($posts))
+        @foreach($posts as $post)
+          <div class="col-lg-4 col-xs-6 thumbnail">
+            <a href="/post/{{ $post->id }}"><img src="{{ asset($post->images[0] ->icon) }}" alt="..." class="img-thumbnail"></a>
+          </div>
+        @endforeach
+      @endif
     </div>
 </div>
 @endsection
